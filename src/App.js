6279d51e9c55cd8127c5f1290a6ed8574aa88984
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-23 11:52:15 -0700

    objects in hooks example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [{count, count2}, setCount] = useState({count: 10, count2: 20}); //
+
+
   return (
     <div className="App">
-        Boilerplate
+      <button onClick={() => setCount(currentCountState => ({ ...currentCountState, count: currentCountState.count + 1}))}>+</button>
+        <div>count 1: {count}</div>
+        <div>count 2: {count2}</div>
     </div>
   );
 }
